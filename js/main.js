10c305a51a63e04e026c7a299e5f21e2d075438b
alexander215 <alexander.hoskinson@colorado.edu>
2019-07-14 18:51:48 -0700

    Node list function with less variables

--- js/main.js
+++ js/main.js
@@ -1,38 +1,32 @@
 const numberDisplay = document.querySelector(".main-display");
-// const controls = document.querySelector(".controls");
 const btn = document.querySelectorAll(".btn");
-const add = document.querySelector(".plus");
-const subtract = document.querySelector(".minus");
 const inputAmount = document.querySelector(".number");
 let initialDisplay = 0;
 
 
 numberDisplay.innerText = initialDisplay;
 
-add.addEventListener('click', e => {
+btn[0].addEventListener('click', e => {
     initialDisplay += parseInt(inputAmount.value);
     numberDisplay.innerText = initialDisplay;
 })
 
-subtract.addEventListener('click', e => {
+btn[1].addEventListener('click', e => {
     initialDisplay -= parseInt(inputAmount.value);
     numberDisplay.innerText = initialDisplay;
 })
 
-add.addEventListener("mouseover", e =>{
-    e.target.style.color = "#a9a9a9";
-    e.target.style.backgroundColor = "#ffffff";
-})
-add.addEventListener("mouseout", e =>{
-    e.target.style.color = "#ffffff";
-    e.target.style.backgroundColor = "#a9a9a9";
-})
+const hoverListener = (node, e) => {
+    for (let i = 0; i < node.length; i ++){
+        node[i].addEventListener("mouseover", e =>{
+            e.target.style.color = "#a9a9a9";
+            e.target.style.backgroundColor = "#ffffff";
+        })
+        node[i].addEventListener("mouseout", e =>{
+            e.target.style.color = "#ffffff";
+            e.target.style.backgroundColor = "#a9a9a9";
+        })
+    }
+}
 
-subtract.addEventListener("mouseover", e =>{
-    e.target.style.color = "#a9a9a9";
-    e.target.style.backgroundColor = "#ffffff";
-})
-subtract.addEventListener("mouseout", e =>{
-    e.target.style.color = "#ffffff";
-    e.target.style.backgroundColor = "#a9a9a9";
-})
+hoverListener(btn);
